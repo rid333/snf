86b77ab4a0d0153eed6ac9f8fe6b323e5e641b15
Rid <alridho2003@gmail.com>
2024-09-10 10:14:01 +0800

    Added navbar

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Logo from "$lib/assets/pngegg.png";
+</script>
+
+<nav
+    class="flex justify-between items-center px-20 font-medium text-lg shadow-md py-5"
+>
+    <img src={Logo} alt="Logo UH" class="w-16 h-16" />
+    <a href="/">Home</a>
+    <a href="/">About</a>
+    <button
+        type="button"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+        Submit
+    </button>
+</nav>
