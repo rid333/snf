de3c018c21078b985c92dc9476a5f65a9f56562d
Rid <alridho2003@gmail.com>
2024-09-25 13:42:47 +0800

    Updated ui

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -14,7 +14,9 @@
     />
 
     <!-- Navbar -->
-    <nav class="flex justify-between items-center p-6 z-10">
+    <nav
+        class="flex text-sm md:text-base lg:text-base justify-between items-center p-6 z-10"
+    >
         <img src={Logo} alt="Fisika" class="w-10 h-10" />
         <ul class="flex space-x-8 font-semibold">
             <li><a href="/" class="hover:text-indigo-400">Home</a></li>
@@ -36,17 +38,17 @@
     <div
         class="flex-grow flex flex-col justify-center items-center px-4 text-center z-10"
     >
-        <h1 class="text-4xl md:text-5xl font-bold mb-4">
+        <h1 class="text-3xl md:text-5xl font-bold mb-4">
             SEMINAR NASIONAL FISIKA (SNF) MAKASSAR 2024
         </h1>
-        <p class="text-lg font-semibold text-gray-400 mb-6">
+        <p class="text-base lg:text-lg font-semibold text-gray-400 mb-6">
             Enhance The Role Of Applied Physics In Renewable Energy Challenges
             For the Utilities Industry
         </p>
         <div class="flex space-x-4">
             <a
                 href="#main"
-                class="flex items-center px-6 py-3 border-2 border-yellow-600 hover:bg-yellow-600 text-white font-semibold"
+                class="flex items-center px-3 py-1.5 lg:px-6 lg:py-3 border-2 border-yellow-600 hover:bg-yellow-600 text-white font-semibold"
                 >Baca Selengkapnya
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
